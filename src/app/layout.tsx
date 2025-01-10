1b9027ab8b6d029e5928ab273655d16cfd12e916
fzl96 <fpsdragon333@gmail.com>
2025-01-10 09:26:16 +0700

    fix: Remove unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Pacifico, Inter } from "next/font/google";
+import { Pacifico, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
